6bc753eda9353ddccec46d57945a59b3fb44e168
chukmunnlee <chukmunnlee@gmail.com>
2022-01-26 15:42:50 +0800

    day02 PM q-learing

---
+++ day02/bj_training_qlearning.py
@@ -0,0 +1,100 @@
+# import all the relevant packages
+import gym, pickle
+import numpy as np
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+#np.random.seed(32)
+
+# tuneables
+GAMMA = 1
+ALPHA = .1
+DECAY = .01
+MIN_PROB = 0.02
+MAX_PROB = 0.9
+PLAYER_MIN_HAND = 16
+PICKLE_FILE_NAME = 'agent-smith-v2.pickle'
+
+EPISODES = 300000
+
+def decay(ep):
+    return MIN_PROB + (MAX_PROB - MIN_PROB) * np.exp(-ep * DECAY)
+
+def policy(st, eps):
+    if np.random.random() > eps:
+        #take the max action
+        return np.argmax(q_table[st])
+
+    # take a random action
+    return np.random.randint(0, 2)
+
+def q_value(st, ac):
+    return q_table[st][ac]
+
+def q_max_value(st):
+    return np.amax(q_table[st])
+
+# create the Blackjack environment
+env = gym.make('Blackjack-v1')
+
+q_table = {}
+for h in range(1, 26):
+    for d in range(1, 11):
+        for a in [ True, False ]:
+            q_table[ (h, d, a) ] = [ 0.0, 0.0 ]
+
+next_action = 0
+win_rate = []
+lose_rate = []
+wins = 0
+
+for e in tqdm(range(EPISODES)):
+    # reset the environemt
+    eps = decay(e)
+    state = env.reset()
+    action = policy(state, eps)
+
+    # reset done
+    done = False
+
+    while not done:
+        new_state, reward, done, _ = env.step(action)
+
+        if done:
+            td_target = reward
+            if reward > 0.9:
+                if state[0] >= PLAYER_MIN_HAND:
+                    wins += 1
+                else:
+                    reward = -1
+            win_rate.append(wins)
+            lose_rate.append(e - wins)
+
+        else: 
+            # one step reward + prediction
+            next_action = policy(new_state, eps)
+            predicted_future_reward = q_max_value(new_state)
+            td_target = reward + (GAMMA * predicted_future_reward)
+
+        #SARSA - state, action, reward, new_state, next_action
+        td_error = td_target - q_value(state, action)
+
+        # update the q_value for state, action
+        q_table[state][action] += ALPHA * td_error
+
+        action = next_action
+        state = new_state
+
+# save the model
+with open(PICKLE_FILE_NAME, 'wb') as f:
+    pickle.dump(q_table, f)
+
+for k, v in enumerate(q_table):
+    print(f'{v} = {q_table[v]}')
+
+## plot the win rate
+#plt.plot(win_rate, label="win rate")
+#plt.plot(lose_rate, label="lose rate")
+#plt.title(f'wins: {win_rate[-1]}')
+#plt.legend()
+#plt.show()
